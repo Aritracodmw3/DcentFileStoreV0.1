2f07a6dd964f965ef0757ea941229ede6f63ab8b
Ubuntu <ubuntu@ip-172-31-28-187.ap-south-1.compute.internal>
2018-10-17 03:36:47 +0000

    pehela

---
+++ build/precache-manifest.1888124ff85c6540bb44c2f90fde5234.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "bcf14171e2a36218fdf5",
+    "url": "/static/css/main.8d15d38f.chunk.css"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8d7728fa1b4f63e5f13",
+    "url": "/static/js/1.b8d7728f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "bcf14171e2a36218fdf5",
+    "url": "/static/js/main.bcf14171.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "b8d7728fa1b4f63e5f13",
+    "url": "/static/css/1.af9a9bc1.chunk.css"
+  },
+  {
+    "revision": "1e9dd982aa5557f195cff7e22f4d337c",
+    "url": "/index.html"
+  }
+];
